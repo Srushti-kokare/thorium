9a6c1c02fe0bce020d84d6bc19736e1e2c04973b
Srushti-kokare <srushtikokare05198@gmail.com>
2022-02-25 01:37:08 +0530

    added query prams

--- src/routes/route.js
+++ src/routes/route.js
@@ -50,36 +50,41 @@ router.post("/post-query-2", function (req, res) {
 // also return an array consisting of only the person that can vote
 
 //  take this as sample for array of persons:
-// let persons= [
-//     {
-//     name: "PK",
-//     age: 10,
-//     votingStatus: false
-// },
-// {
-//     name: "SK",
-//     age: 20,
-//     votingStatus: false
-// },
-// {
-//     name: "AA",
-//     age: 70,
-//     votingStatus: false
-// },
-// {
-//     name: "SC",
-//     age: 5,
-//     votingStatus: false
-// },
-// {
-//     name: "HO",
-//     age: 40,
-//     votingStatus: false
-// }
-// ]
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+},
+{
+    name: "SK",
+    age: 20,
+    votingStatus: false
+},
+{
+    name: "AA",
+    age: 70,
+    votingStatus: false
+},
+{
+    name: "SC",
+    age: 5,
+    votingStatus: false
+},
+{
+    name: "HO",
+    age: 40,
+    votingStatus: false
+}
+]
 
+router.post("/voters", function(req, res){
+    const votingAge = req.query.votingAge
+    console.log(req.query)
+    const personVotingStatus = persons.filter(element => element.age > votingAge).map(element => ({...element, votingStatus : true}))
+    console.log(personVotingStatus)
+    res.send(personVotingStatus)
 
-
-
+})
 
 module.exports = router;
