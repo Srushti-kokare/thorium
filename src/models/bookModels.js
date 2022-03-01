781d49bdd7c31a831ab67f382c604334df14282b
Srushti-kokare <srushtikokare05198@gmail.com>
2022-03-01 15:33:57 +0530

    mongodb part 2 added

---
+++ src/models/bookModels.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    "bookName": {
+        type: String,
+        required: true
+    },
+    "authorName": {
+        type: String,
+        required: true
+    },
+    category: String,
+    year: Number,
+    INR: Number,
+    pages: Number,
+    inStock : Boolean
+}, { timestamps: true });
+
+module.exports = mongoose.model('BookInfo', bookSchema)
