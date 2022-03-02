2230e16160780325f26bbe1aa544ed1993ccf7ab
Srushti-kokare <srushtikokare05198@gmail.com>
2022-03-03 01:13:29 +0530

    populated n reference

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://srushti_kokare:srushti5198@cluster0.sybm5.mongodb.net/srushtidb?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
