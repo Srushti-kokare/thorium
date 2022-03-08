a40551a295fad4d043f55e646ecfcd1fc0aebd20
Srushti-kokare <srushtikokare05198@gmail.com>
2022-03-08 23:00:00 +0530

    jws token added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://srushti_kokare:srushti5198@cluster0.sybm5.mongodb.net/srushtidb?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
